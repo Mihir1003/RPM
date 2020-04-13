337b7a8c5fa654b7f84283acf08f0398e326540d
Mihir1003 <32996071+Mihir1003@users.noreply.github.com>
2020-04-13 20:14:55 +0530

    v1.0

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 from tkinter import *
 from tasks import *
-createTask("test","4-1-2020")
+import json
 
+f=open("tasks.json","w")
+f.write("")
+f.close()
 window = Tk()
 window.title("Welcome to Repl.it")
 window.geometry('350x200')
 
-lbl = Label(window, text="Hello")
-lbl.grid(column=0, row=0)
+lbl = Label(window, text="Empty")
+lbl.grid(column=0, row=1)
 
 txt = Entry(window,width=10)
 txt.grid(column=1, row=0)
 
 def clicked():
-    lbl.configure(text="Button was clicked !!")
+    createTask(txt.get())
+    f = open("tasks.json", "r")
+    if f.read():
+        f.seek(0)
+        tasks = [value["title"] for value in list(json.loads(f.read()).values())]
+    else:
+        tasks = ""
+    f.close()
+    lbl.configure(text=tasks)
 
-btn = Button(window, text="Click Me", command=clicked)
+btn = Button(window, text="add", command=clicked)
 btn.grid(column=2, row=0)
 
 
--- tasks.py
+++ tasks.py
@@ -1,16 +1,21 @@
 import json
-def createTask(taskTitle,taskEndDate,taskSatus="completed"):
-  f=open("tasks.json","a+")
-  
-  
-
-  f.write(json.dumps({"title":taskTitle,"taskEndDate":taskEndDate})+",")
-
-  s=json.loads(f.read())
-  print(s)
-
-  f.close()
 
 
+def createTask(taskTitle):
+    f = open("tasks.json", "r+")
 
-  
+    f.seek(0)
+    s = (f.read())
+    if not s:
+        d = {}
+    else:
+        d = json.loads(s)
+    if d.keys():
+        maxKey = int(max(d.keys()))
+    else:
+        maxKey = 0
+    print(s)
+    d[maxKey + 1] = {"title": taskTitle}
+    f.seek(0)
+    f.write(json.dumps(d))
+    f.close()
